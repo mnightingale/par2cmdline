611db8d452a75337ecb9aeaaaa703fcd17480a26
BlackEagle <ike.devolder@gmail.com>
2026-08-16 15:09:07 +0200

    fix progress getting stalled in ScanDataFile
    When Step or Jump have been used the progress would be stalled because
    the progress is no longer exactly the same as blocksize
    
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- src/par2repairer.cpp
+++ src/par2repairer.cpp
@@ -1653,7 +1653,7 @@ bool Par2Repairer::ScanDataFile(DiskFile                *diskfile,    // [in]
     {
       // Update progress indicator
       printprogress += filechecksummer.Offset() - oldoffset;
-      if (printprogress == blocksize || filechecksummer.ShortBlock())
+      if (printprogress >= blocksize || filechecksummer.ShortBlock())
       {
         progress.Add(printprogress);
         printprogress = 0;
